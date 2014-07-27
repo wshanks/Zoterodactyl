b48c394b2f174b98781cdbf6b59645134fd361f5
Will S <wsha.code@gmail.com>
2014-07-27 19:35:26 -0400

    Build README without adding new lines on ex tags.

--- build/parse_xml.py
+++ build/parse_xml.py
@@ -1,21 +1,47 @@
 #!/usr/bin/env python3
+"""
+Script for parsing zoterodactyl.xml (Zoterodactyl section of Pentadactyl's
+plugins.xml file).
+"""
 from bs4 import BeautifulSoup
 
 def clean_text(text_limb):
-    clean_str = ''
+    """
+    Clean up descriptive text XML limb by:
+    * Replacing <ex> tags with their text contents wrapped with '`'.
+    * Concatenating <p> tags with '\n\n'.
+    * Stripping other tags.
+    """
+    clean_strings = []
+    # loop on p tags, loop on ex tags -- insert_before and then decompose them
+    # join all p tags with \n\n
+    for p_tag in text_limb.find_all('p'):
+        for ex_tag in p_tag.find_all('ex'):
+            ex_tag.insert_before('`' + ex_tag.get_text() + '`')
+            ex_tag.decompose()
 
-    # Use for tag in find_all; new_string; append
+        clean_strings.append(p_tag.get_text('', strip=True))
 
-with open('zoterodactyl.xml', 'r') as f:
-    xml = f.read()
+    return '\n\n'.join(clean_strings)
 
-soup = BeautifulSoup(markup=xml)
+def main():
+    """
+    Main function called when script is run
+    """
+    with open('zoterodactyl.xml', 'r') as xml_file:
+        xml = xml_file.read()
 
-with open('introduction.md', 'w') as f:
-    f.write('{0}\n\n'.format(soup.plugin.findNext('p').getText('\n\n')))
+    soup = BeautifulSoup(markup=xml)
 
-with open('commands.md', 'w') as f:
-    for item in soup.findAll('item'):
-        clean_text = item.description.getText('\n\n', strip=True)
-        f.write('__{0}:__ {1}\n\n'.format(item.spec.text, clean_text))
+    with open('introduction.md', 'w') as intro:
+        intro_text = soup.plugin.find_next('p').get_text('\n\n')
+        intro.write('{0}\n\n'.format(intro_text))
 
+    with open('commands.md', 'w') as commands:
+        for item in soup.findAll('item'):
+            description = clean_text(item)
+            commands.write('__{0}:__ {1}\n\n'.format(item.spec.text,
+                                                     description))
+
+if __name__ == "__main__":
+    main()
